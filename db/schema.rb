fca7e6bce03c7e91db5215f825a750c24879593c
iposton <ianposton@sbcglobal.net>
2015-04-01 16:16:40 -0700

    create chef and recipe associations and tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401073221) do
+ActiveRecord::Schema.define(version: 20150401224130) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150401073221) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "chef_id"
   end
 
 end
---
+++ test/models/recipe_test.rb
@@ -0,0 +1,64 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+	def setup
+		@chef = Chef.create(chefname: "bob", email: "bob@example.com")
+		@recipe = @chef.recipes.build(name: "Chicken Parm", summary: "This is the best chicken parm recipe ever", description: "I make this all the time. I had 2 pounds of chicken and I cook it for 45 minutes. And it is oh so good.")
+	end
+
+	test "recipe should be valid" do
+		assert @recipe.valid?
+	end
+
+	test "chef id should be present" do
+		@recipe.chef_id = nil
+		assert_not @recipe.valid?
+
+	end
+
+	test "name should be present" do
+		@recipe.name = " "
+		assert_not @recipe.valid?
+	end
+
+	test "name length should not be too long" do
+		@recipe.name = "a" * 101
+		assert_not @recipe.valid?
+	end
+
+	test "name length should not be too short" do
+		@recipe.name = "aaaa"
+		assert_not @recipe.valid?
+	end
+
+	test "summary should be present" do
+		@recipe.summary = " "
+		assert_not @recipe.valid?
+	end
+
+	test "summary length should not be too long" do
+		@recipe.summary = "a" * 151
+		assert_not @recipe.valid?
+	end
+
+	test "summary length should not be too short" do
+		@recipe.summary = "a" * 9
+		assert_not @recipe.valid?
+	end
+
+	test "description should be present" do
+		@recipe.description = " "
+		assert_not @recipe.valid?
+	end
+
+	test "description length should not be too short" do
+		@recipe.description = "a" * 501
+		assert_not @recipe.valid?
+	end
+
+	test "description length should not be too long" do
+		@recipe.description = "a" * 19
+		assert_not @recipe.valid?
+	end
+
+end
